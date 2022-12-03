dbddb463320f36369cb691e34ee18335358f1117
devliss <liseyna1@gmail.com>
2022-12-03 13:42:39 +0300

    fixed ban user by sa

--- src/features/api/super-admin/users/application/useCases/banUser.useCase.ts
+++ src/features/api/super-admin/users/application/useCases/banUser.useCase.ts
@@ -4,6 +4,7 @@ import { UserRepository } from '../../../../../entities/mongo/user/infrastructur
 import { UserQueryRepository } from '../../../../../entities/mongo/user/infrastructure/user-query.repository';
 import { CommentsRepository } from '../../../../../entities/mongo/comment/infrastucture/comments.repository';
 import { SessionsService } from '../../../../public/sessions/application/sessions.service';
+import { UserSqlRepository } from '../../../../../entities/postgres/userSql.repository';
 
 export class BanUserCommand {
   constructor(public id: string, public banDto: BanDto) {}
@@ -11,8 +12,9 @@ export class BanUserCommand {
 @CommandHandler(BanUserCommand)
 export class BanUserUseCase implements ICommandHandler<BanUserCommand> {
   constructor(
-    private userRepo: UserRepository,
-    private userQueryRepo: UserQueryRepository,
+    // private userRepo: UserRepository,
+    //private userQueryRepo: UserQueryRepository,
+    private userRepo: UserSqlRepository,
     private commentRepo: CommentsRepository,
     private sessionService: SessionsService,
   ) {}
@@ -20,7 +22,7 @@ export class BanUserUseCase implements ICommandHandler<BanUserCommand> {
   async execute(command: BanUserCommand): Promise<any> {
     const bannedUser = await this.userRepo.banUser(command.id, command.banDto);
     console.log(bannedUser);
-    const comments = await this.commentRepo.updateUserInfo(bannedUser);
+    //const comments = await this.commentRepo.updateUserInfo(bannedUser);
     const session = await this.sessionService.removeUserSessions(command.id);
 
     return bannedUser;
