5737f0a38d47ad56c79eb382829d3fe3d4d9d433
devliss <liseyna1@gmail.com>
2022-12-03 18:03:33 +0300

    fixed delete user

--- src/features/api/super-admin/users/application/useCases/deleteUser.useCase.ts
+++ src/features/api/super-admin/users/application/useCases/deleteUser.useCase.ts
@@ -1,5 +1,6 @@
 import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
 import { UserRepository } from '../../../../../entities/mongo/user/infrastructure/user.repository';
+import { UserSqlRepository } from '../../../../../entities/postgres/userSql.repository';
 
 export class DeleteUserCommand {
   constructor(public id: string) {}
@@ -7,7 +8,7 @@ export class DeleteUserCommand {
 
 @CommandHandler(DeleteUserCommand)
 export class DeleteUserUseCase implements ICommandHandler<DeleteUserCommand> {
-  constructor(private userRepo: UserRepository) {}
+  constructor(private userRepo: UserSqlRepository) {}
 
   execute(command: DeleteUserCommand): Promise<any> {
     return this.userRepo.deleteUser(command.id);
