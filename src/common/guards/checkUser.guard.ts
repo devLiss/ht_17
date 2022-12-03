bf9190d658fd25cd7832dfbd6866d9535a8f69d0
devliss <liseyna1@gmail.com>
2022-12-03 19:33:25 +0300

    fixed delete user by sa

--- src/common/guards/checkUser.guard.ts
+++ src/common/guards/checkUser.guard.ts
@@ -5,12 +5,15 @@ import {
   NotFoundException,
 } from '@nestjs/common';
 import { UserQueryRepository } from '../../features/entities/mongo/user/infrastructure/user-query.repository';
+import { UserSqlRepository } from '../../features/entities/postgres/userSql.repository';
 @Injectable()
 export class CheckUserGuard implements CanActivate {
-  constructor(private userQueryRepo: UserQueryRepository) {}
+  constructor(
+    private userQueryRepo: UserSqlRepository /*UserQueryRepository*/,
+  ) {}
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const req = context.switchToHttp().getRequest();
-    const user = await this.userQueryRepo.findById(req.params.id);
+    const user = await this.userQueryRepo.getUserById(req.params.id);
     if (!user) throw new NotFoundException();
 
     return true;
