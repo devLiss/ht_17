76f8a65c0e3619c137c54eb0cd27b614557ed657
devliss <aaandruk@gmail.com>
2022-12-11 23:53:46 +0300

    fixed update post by blogger. added check if post exists

--- src/features/api/bloggers/blogs/application/useCases/updatePost.useCase.ts
+++ src/features/api/bloggers/blogs/application/useCases/updatePost.useCase.ts
@@ -1,6 +1,7 @@
 import { CommandBus, CommandHandler, ICommandHandler } from '@nestjs/cqrs';
 import { PostInputModelDto } from '../../dto/postInputModel.dto';
 import { PostSqlRepository } from '../../../../../entities/postgres/postSql.repository';
+import { NotFoundException } from '@nestjs/common';
 
 export class UpdatePostCommand {
   constructor(public id: string, public cpDto: PostInputModelDto) {}
@@ -9,7 +10,9 @@ export class UpdatePostCommand {
 @CommandHandler(UpdatePostCommand)
 export class UpdatePostUseCase implements ICommandHandler<UpdatePostCommand> {
   constructor(private postRepo: PostSqlRepository) {}
-  execute(command: UpdatePostCommand): Promise<any> {
+  async execute(command: UpdatePostCommand): Promise<any> {
+    const post = await this.postRepo.getPostById(command.id);
+    if (!post) throw new NotFoundException();
     return this.postRepo.updatePost(command.id, command.cpDto);
   }
 }
