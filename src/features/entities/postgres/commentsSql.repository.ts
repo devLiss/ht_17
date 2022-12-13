ef1892ac4f6f02283c6967e75b2a31c0481ae043
devliss <aaandruk@gmail.com>
2022-12-14 00:28:49 +0300

    fixed dislike and like counts type (to Number)

--- src/features/entities/postgres/commentsSql.repository.ts
+++ src/features/entities/postgres/commentsSql.repository.ts
@@ -32,8 +32,8 @@ export class CommentsSqlRepository {
         userLogin: item.userLogin,
         createdAt: item.createdAt,
         likesInfo: {
-          likesCount: item.likesCount,
-          dislikesCount: item.dislikesCount,
+          likesCount: +item.likesCount,
+          dislikesCount: +item.dislikesCount,
           myStatus: item.myStatus,
         },
       };
@@ -91,8 +91,8 @@ export class CommentsSqlRepository {
         userLogin: item.userLogin,
         createdAt: item.createdAt,
         likesInfo: {
-          likesCount: item.likesCount,
-          dislikesCount: item.dislikesCount,
+          likesCount: +item.likesCount,
+          dislikesCount: +item.dislikesCount,
           myStatus: item.myStatus,
         },
       };
