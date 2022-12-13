ab5c78a2be36b366de457f5590fc9504341062de
devliss <aaandruk@gmail.com>
2022-12-14 00:36:17 +0300

    fixed myStatus naming

--- src/features/entities/postgres/postSql.repository.ts
+++ src/features/entities/postgres/postSql.repository.ts
@@ -46,7 +46,7 @@ export class PostSqlRepository {
     const query = `select 
       p.*, b.name as "blogName", (select row_to_json(x2) from (select * from (select count(*) as "likesCount"  from likes l where l."likeableType" ='post' and l.status = 'Like' and l."likeableId" = p.id ) as likesCount ,
       (select count(*) as "dislikesCount" from likes l where l."likeableType" ='post' and l.status = 'Dislike' and l."likeableId" =p.id ) as dislikesCount ,
-      coalesce((select  l.status as "myStatus" from likes l where l."likeableType" ='post' and l."likeableId" = p.id and l."userId" = ${userForLikes}  ),'None') as myStatus ) x2)as "extendedLikesInfo",
+      coalesce((select  l.status as "myStatus" from likes l where l."likeableType" ='post' and l."likeableId" = p.id and l."userId" = ${userForLikes}  ),'None') as "myStatus" ) x2)as "extendedLikesInfo",
       (select array_to_json(array_agg( row_to_json(t))) from (select l2."createdAt" as "addedAt" , l2."userId" as "userId" ,u.login as login
       from likes l2 left join users u on l2."userId" = u.id where l2.status = 'Like' and l2."likeableType"  = 'post'
       and l2."likeableId"  = p.id order by l2."createdAt" desc limit 3) t) as "newestLikes" 
@@ -88,7 +88,7 @@ export class PostSqlRepository {
     const query = `select 
       p.*, b.name as "blogName", (select row_to_json(x2) from (select * from (select count(*) as "likesCount"  from likes l where l."likeableType" ='post' and l.status = 'Like' and l."likeableId" = p.id ) as likesCount ,
       (select count(*) as "dislikesCount" from likes l where l."likeableType" ='post' and l.status = 'Dislike' and l."likeableId" =p.id ) as dislikesCount ,
-      coalesce((select  l.status as "myStatus" from likes l where l."likeableType" ='post' and l."likeableId" = p.id and l."userId" = ${userForLikes}  ),'None') as myStatus ) x2)as "extendedLikesInfo",
+      coalesce((select  l.status as "myStatus" from likes l where l."likeableType" ='post' and l."likeableId" = p.id and l."userId" = ${userForLikes}  ),'None') as "myStatus" ) x2)as "extendedLikesInfo",
       (select array_to_json(array_agg( row_to_json(t))) from (select l2."createdAt" as "addedAt" , l2."userId" as "userId" ,u.login as login
       from likes l2 left join users u on l2."userId" = u.id where l2.status = 'Like' and l2."likeableType"  = 'post'
       and l2."likeableId"  = p.id order by l2."createdAt" desc limit 3) t) as "newestLikes" 
@@ -136,7 +136,7 @@ export class PostSqlRepository {
     const query = `select 
       p.*, b.name as "blogName", (select row_to_json(x2) from (select * from (select count(*) as "likesCount"  from likes l where l."likeableType" ='post' and l.status = 'Like' and l."likeableId" = p.id ) as likesCount ,
       (select count(*) as "dislikesCount" from likes l where l."likeableType" ='post' and l.status = 'Dislike' and l."likeableId" =p.id ) as dislikesCount ,
-      coalesce((select  l.status as "myStatus" from likes l where l."likeableType" ='post' and l."likeableId" = p.id and l."userId" = ${userForLikes}  ),'None') as myStatus ) x2)as "extendedLikesInfo",
+      coalesce((select  l.status as "myStatus" from likes l where l."likeableType" ='post' and l."likeableId" = p.id and l."userId" = ${userForLikes}  ),'None') as "myStatus" ) x2)as "extendedLikesInfo",
       (select array_to_json(array_agg( row_to_json(t))) from (select l2."createdAt" as "addedAt" , l2."userId" as "userId" ,u.login as login
       from likes l2 left join users u on l2."userId" = u.id where l2.status = 'Like' and l2."likeableType"  = 'post'
       and l2."likeableId"  = p.id order by l2."createdAt" desc limit 3) t) as "newestLikes" 
